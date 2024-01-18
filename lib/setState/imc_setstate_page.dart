5184f2dfecd8d759667491fc67abac95b3ed0a73
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-01-18 20:34:17 -0300

    y

--- lib/setState/imc_setstate_page.dart
+++ lib/setState/imc_setstate_page.dart
@@ -1,22 +1,32 @@
+import 'dart:math';
+
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../widgets/imc_gauge.dart';
 
 class ImcSetstatePage extends StatefulWidget {
-
-  const ImcSetstatePage({ super.key });
+  const ImcSetstatePage({super.key});
 
   @override
   State<ImcSetstatePage> createState() => _ImcSetstatePageState();
 }
 
 class _ImcSetstatePageState extends State<ImcSetstatePage> {
-
   // preciso recuperar os valores informados nos campos de peso e altura.
   final pesoEC = TextEditingController();
   final alturaEC = TextEditingController();
+  final formkey = GlobalKey<FormState>();
   var imc = 0.0;
+
+  //metodo que calcula o imc
+  void _calcularIMC({required double peso, required double altura}) {
+    // colocamos essa função setState que é a classe que chamamos para a pagina ser rebildada.
+    setState(() {
+      imc = peso / pow(altura, 2);
+    });
+  }
 
   // descartar os valores captados
   @override
@@ -26,52 +36,91 @@ class _ImcSetstatePageState extends State<ImcSetstatePage> {
     super.dispose();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IMC SETSTATE'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formkey, //como usamos form precisamos realizar uma chave
+          child: Padding(
+            // PADDIN USADO PARA DAR ESPAÇO GERAL NA PAGINA
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                ImcGauge(imc: imc),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  decoration: const InputDecoration(labelText: 'Peso'),
+                  keyboardType:
+                      TextInputType.number, // coloca o teclado como numerico
+                  inputFormatters: [
+                    //formatamos os numeros desse imput usando packged baixado
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR', //qual teclado vai usar
+                      symbol: '',
+                      turnOffGrouping: true, //sem o ponto decimal entre os mil
+                      decimalDigits: 2, //quantidade decimal
+                    )
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Peso Obrigatorio';
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  decoration: const InputDecoration(labelText: 'Altura'),
+                  keyboardType:
+                      TextInputType.number, // coloca o teclado como numerico
+                  inputFormatters: [
+                    //formatamos os numeros desse imput usando packged baixado
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR', //qual teclado vai usar
+                      symbol: '',
+                      turnOffGrouping: true, //sem o ponto decimal entre os mil
+                      decimalDigits: 2, //quantidade decimal
+                    )
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Altura Obrigatoria';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var formValid = formkey.currentState?.validate() ?? false;
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('IMC SETSTATE'),),
-           body: SingleChildScrollView(
-            child: Padding( // PADDIN USADO PARA DAR ESPAÇO GERAL NA PAGINA
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  ImcGauge(imc: imc),
-                  const SizedBox(height: 20,),
-                  TextFormField(
-                    controller: pesoEC,
-                    decoration: const InputDecoration(labelText: 'Peso'),
-                    keyboardType: TextInputType.number, // coloca o teclado como numerico
-                    inputFormatters: [ //formatamos os numeros desse imput usando packged baixado
-                      CurrencyTextInputFormatter(
-                        locale: 'pt_BR', //qual teclado vai usar
-                        symbol: '', 
-                        turnOffGrouping: true, //sem o ponto decimal entre os mil
-                        decimalDigits: 2, //quantidade decimal
-
-                      )
-                    ],
-                  ),
-                  TextFormField(
-                    controller: alturaEC,
-                    decoration: const InputDecoration(labelText: 'Altura'),
-                    keyboardType: TextInputType.number, // coloca o teclado como numerico
-                    inputFormatters: [ //formatamos os numeros desse imput usando packged baixado
-                      CurrencyTextInputFormatter(
-                        locale: 'pt_BR', //qual teclado vai usar
-                        symbol: '', 
-                        turnOffGrouping: true, //sem o ponto decimal entre os mil
-                        decimalDigits: 2, //quantidade decimal
-
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 20,),
-                  ElevatedButton(onPressed: (){}, child: const Text('Calcular IMC'))
-                ],
-
-              ),),
-           ),
-       );
+                      if (formValid) {
+                        // converter e usamos o pacote intl 0.17.0
+                        var formater = NumberFormat.simpleCurrency(
+                          locale: 'pt_BR',
+                          decimalDigits: 2,
+                        );
+                        double peso = formater.parse(pesoEC.text)
+                            as double;
+                        double altura = formater.parse(alturaEC.text)
+                            as double; //como o dado vem em string precisamos
+                        
+                        _calcularIMC(peso: peso, altura: altura);
+                      }
+                    },
+                    child: const Text('Calcular IMC'))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
